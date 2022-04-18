5f2510f02cb1965415d681f2591efceafe7cf81c
Akeju Paul <akejupaul10@gmail.com>
2022-04-18 05:28:17 +0100

    COmpleted the auths

--- screens/auth/ForgotPasswordEmail/ForgotPasswordEmail.js
+++ screens/auth/ForgotPasswordEmail/ForgotPasswordEmail.js
@@ -1,46 +1,39 @@
 import React from "react";
 import {View,Text,StyleSheet,Image,TextInput,TouchableOpacity} from 'react-native'
 import { colors,fonts,sizes } from "../../../constant";
-import password from '../../../assets/images/password.png'
+import email_check from '../../../assets/images/email_check.png'
 
 
 
-function ForgotPassword({navigation}){
-    const [text,onChangeText]=React.useState('')
+function ForgotPasswordEmail({route,navigation}){
     const forgot=forgotStyle
+    const { email } = route.params;
     return (
         <View style={forgot.container}>
-            <Text style={forgot.forgot_text_1}>Forgot password?</Text>
-            <Text style={forgot.forgot_text_2}>To reset your password, you need your email or mobile number that can be authenticated</Text>
+            <Text style={forgot.forgot_text_1}>Check Your Email</Text>
+            <Text style={forgot.forgot_text_2}>We have sent the reset password to the email address {email}</Text>
 
             <View style={{width:'100%',display:'flex',justifyContent:'center',alignItems:'center',height:'30%'}}>
-                <Image style={{height:130}} resizeMode={'contain'} resizeMethod={'scale'} source={password} />
+                <Image style={{height:130}} resizeMode={'contain'} resizeMethod={'scale'} source={email_check} />
             </View>
 
-            <View style={{width:'100%'}}>
-            <Text style={forgot.forgot_email_label}>Email</Text>
-                <TextInput value={text} onChangeText={onChangeText} keyboardType="email-address" style={forgot.forgot_email_input} placeholder="Enter valid email address" placeholderTextColor={'#c0c0c0'} />
-            </View>
-
+          
             <View style={{width:'100%',display:'flex',justifyContent:'center',alignItems:'center',marginTop:20}}>
-            <TouchableOpacity onPress={()=>{if(text==''){
-
-            }else{
-                navigation.navigate('ForgotPasswordEmail',{
-                    email:text
-                })
-            }}} activeOpacity={0.8} style={[forgot.button, { backgroundColor: colors.primary }]}>
-                    <Text style={{ color: 'white', fontFamily: fonts.DmSans_Bold, fontSize: sizes.h14 }}>RESET PASSWORD</Text>
+            <TouchableOpacity onPress={()=>{navigation.navigate('ForgotPasswordSuccess')}} activeOpacity={0.8} style={[forgot.button, { backgroundColor: colors.primary }]}>
+                    <Text style={{ color: 'white', fontFamily: fonts.DmSans_Bold, fontSize: sizes.h14 }}>OPEN YOUR EMAIL</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity onPress={()=>{navigation.navigate('Login')}} activeOpacity={0.8} style={[forgot.button, { backgroundColor: colors.tertiary }]}>
                     <Text style={{ color: 'white', fontFamily: fonts.DmSans_Bold, fontSize: sizes.h14 }}>BACK TO LOGIN</Text>
                 </TouchableOpacity>
             </View>
+            <View>
+                <Text style={{ fontFamily: fonts.DmSans_Regular, fontSize: sizes.h12 }}>You have not received an email? <Text onPress={()=>{navigation.navigate('ForgotPasswordEmail')}} style={{ color: colors.ultra }}>Resend</Text></Text>
+            </View>
         </View>
     )
 }
-export default ForgotPassword;
+export default ForgotPasswordEmail;
 
 const forgotStyle=StyleSheet.create({
     container: {

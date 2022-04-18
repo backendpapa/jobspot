d86faef922a6da25b432bdc0b4089c8534e14d1f
Akeju Paul <akejupaul10@gmail.com>
2022-04-18 04:30:45 +0100

    completed the signup screen

---
+++ screens/auth/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View,Text} from 'react-native'
+
+
+
+function ForgotPassword(){
+    return (
+        <View>
+            <Text>Forgot password</Text>
+        </View>
+    )
+}
+export default ForgotPassword;
